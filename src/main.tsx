5cb38bfefbc513de325d7ada29e20ccdbfbb6c95
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-10-10 13:14:27 -0300

    feat(router): adiciona Outlet ao App para renderizar rotas filhas
    - Configurado App.tsx com <Outlet /> para exibir páginas internas
    
    - Estrutura de rotas criada em main.tsx com Home como rota inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Pages
+import Home from "./routes/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );

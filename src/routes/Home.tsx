5f9172cf1c288ffa17dd18c85fd17080aba4864e
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-10-11 12:04:41 -0300

    refactor(home): adiciona estado user tipado com UserProps

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Search from "../components/Search";
+import type { UserProps } from "../types/user";
 
 const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
   return (
     <div>
       <Search />

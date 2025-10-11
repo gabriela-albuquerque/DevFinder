9defa9889e5005673fd49b7b97cd4ff1ade2e382
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-10-11 13:18:01 -0300

    feat(home): implementa lógica inicial de busca de usuário no GitHub
    - Criada função loadUser para buscar dados da API do GitHub
    
    - Passada função como prop para o componente Search
    
    - Componente Search agora captura input e chama loadUser ao clicar no botão

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,9 +4,15 @@ import type { UserProps } from "../types/user";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
     </div>
   );
 };

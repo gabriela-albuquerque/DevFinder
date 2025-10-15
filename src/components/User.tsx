fe81f5fa45f187f0f167c3676e31ecac8b69d350
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-10-15 14:55:45 -0300

    style(user): cria e implementa módulo de estilização do componente user

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,7 +1,7 @@
 import { MdLocationPin } from "react-icons/md";
 import type { UserProps } from "../types/user";
 import { Link } from "react-router-dom";
-import { FaUsers } from "react-icons/fa";
+import classes from "./User.module.css";
 
 const User = ({
   login,
@@ -11,27 +11,26 @@ const User = ({
   location,
 }: UserProps) => {
   return (
-    <div>
+    <div className={classes.user}>
       <img src={avatar_url} alt={login} />
       <h2>{login}</h2>
       {location && (
-        <p>
+        <p className={classes.location}>
           <MdLocationPin />
           <span>{location}</span>
         </p>
       )}
-      <div>
+      <div className={classes.stats}>
         <div>
-          <FaUsers />
           <p>Seguidores:</p>
-          <p>{followers}</p>
+          <p className={classes.number}>{followers}</p>
         </div>
         <div>
           <p>Seguindo:</p>
-          <p>{following}</p>
+          <p className={classes.number}>{following}</p>
         </div>
       </div>
-      <Link to={`/repos/${login}`}>Coonheça os projetos com mais estrelas</Link>
+      <Link to={`/repos/${login}`}>Projetos com mais estrelas</Link>
     </div>
   );
 };
